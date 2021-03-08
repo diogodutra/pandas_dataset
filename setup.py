0c6ca1f7d46fdf004e31a7691728084d1c46d784
Diogo Dutra <diogodutra@gmail.com>
2021-03-08 07:43:52 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,67 +1,26 @@
-# This is purely the result of trial and error.
+import os
+from setuptools import setup
 
-import sys
-import codecs
+def read(fname):
+    try:
+        with open(os.path.join(os.path.dirname(__file__), fname)) as fh:
+            return fh.read()
+    except IOError:
+        return ''
 
-from setuptools import setup, find_packages
+requirements = read('requirements.txt').splitlines()
 
-import pdds
-
-
-install_requires = [
-    'requests[socks]>=2.22.0',
-    'Pygments>=2.5.2',
-    'requests-toolbelt>=0.9.1',
-]
-install_requires_win_only = [
-    'colorama>=0.2.4',
-]
-
-# Conditional dependencies:
-
-# sdist
-if 'bdist_wheel' not in sys.argv:
-
-    if 'win32' in str(sys.platform).lower():
-        # Terminal colors for Windows
-        install_requires.extend(install_requires_win_only)
-
-
-# bdist_wheel
-extras_require = {
-    # https://wheel.readthedocs.io/en/latest/#defining-conditional-dependencies
-    ':sys_platform == "win32"': install_requires_win_only,
-}
-
-
-def long_description():
-  return 'Long description'
-
-
-setup(
-    name='pdds',
-    download_url='https://github.com/diogodutra/pandas_dataset/archive/main.tar.gz',
-    packages=find_packages(),
-    python_requires='>=3.6',
-    extras_require=extras_require,
-    install_requires=install_requires,
-    tests_require=tests_require,
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3 :: Only',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: BSD License',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Software Development',
-        'Topic :: System :: Networking',
-        'Topic :: Terminals',
-        'Topic :: Text Processing',
-        'Topic :: Utilities'
-    ],
-    project_urls={
-        'GitHub': 'https://github.com/diogodutra/pandas_dataset',
-    },
-)
+setup(name='pdds',
+      version='0.0.1',
+      description='Pandas extension for analysis of dataset for classification',
+      url='https://github.com/diogodutra/pandas_dataset',
+      author='Diogo Dutra',
+      author_email='diogodutra@gmail.com',
+      license='Apache License, Version 2.0',
+      packages=['pdds'],
+      package_data={
+        'pyAudioAnalysis': ['dataset.py']
+      },
+      zip_safe=False,
+      install_requires=requirements,
+      )
