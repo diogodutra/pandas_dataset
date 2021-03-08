c1b3e354865b83d4fa310d0b26b02dcf59bad102
Diogo Dutra <diogodutra@gmail.com>
2021-03-08 08:01:46 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,14 @@
-import os
-from setuptools import setup
 
-def read(fname):
-    try:
-        with open(os.path.join(os.path.dirname(__file__), fname)) as fh:
-            return fh.read()
-    except IOError:
-        return ''
-
-requirements = read('requirements.txt').splitlines()
+from setuptools import setup, find_packages
 
 setup(name='pdds',
-      version='0.0.1',
-      description='Pandas extension for analysis of dataset for classification',
-      url='https://github.com/diogodutra/pandas_dataset',
-      author='Diogo Dutra',
-      author_email='diogodutra@gmail.com',
-      license='Apache License, Version 2.0',
-      packages=['pdds'],
-      package_data={
-        'pyAudioAnalysis': ['dataset.py']
-      },
-      zip_safe=False,
-      install_requires=requirements,
-      )
+    version='0.0.1',
+    description='Pandas expansion to analyze dataset for classifier',
+    author='diogodutra',
+    url='https://github.com/diogodutra/pandas_dataset',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'matplotlib'
+    ]
+)
